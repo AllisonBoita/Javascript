6ff0d6344c65dd1071a1a01fdde6671935645f55
Allison Cristiano Boita <allison.boita@expressosaomiguel.com.br>
2024-09-16 08:15:02 -0300

    FEAT: Aula 17 - FOR

---
+++ Aula17_52/index.js
@@ -0,0 +1,11 @@
+//for (let i = 0; i <= 5; i++) {
+//    console.log(i)
+//}
+
+// precisamos declarar a variavel, a condição e a adição ou subtração da variável.
+
+for (let i = 0; i <= 10; i++) {
+    const parImpar = i % 2 === 0 ? 'par' : 'impar'
+    console.log('o numero', i , 'é', parImpar)
+}
+
