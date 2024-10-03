08dbbfa2cd669698f3eac717509073eac9f0d3c7
Allison Cristiano Boita <61565817+AllisonBoita@users.noreply.github.com>
2024-10-03 10:47:38 -0300

    FEAT: Finalizando Aula Agenda

--- Aula25_67/assets/js/main.js
+++ Aula25_67/assets/js/main.js
@@ -15,13 +15,64 @@ inputNovaTarefa.addEventListener('keypress', function(evento){
     }
 })
 
+function limpaInput() {
+    inputNovaTarefa.value = ''
+    inputNovaTarefa.focus()
+}
+
+function criaBotaoApagar(li)  { //preciso receber um li pra saber onde vou adicionar o botão
+    li.innerText += ' '
+    const botaoApagar = document.createElement('button')
+    botaoApagar.innerText = 'Apagar'
+    botaoApagar.setAttribute('class', 'apagar')
+    li.appendChild(botaoApagar)
+}
+
 function criaTarefa(textoInput) {
     const li = criarLi()
     li.innerText = textoInput
     tarefas.appendChild(li)
+    limpaInput()
+    criaBotaoApagar(li)
+    salvarTarefas()
 }
 
 btnAddTarefa.addEventListener('click', function(evento){
     if (!inputNovaTarefa.value) return;
     criaTarefa(inputNovaTarefa.value)
 })
+
+document.addEventListener('click', function(e){ //capturo o evento pra saber onde na minha pagina está sendo clicado
+    const elemento = e.target
+
+    if (elemento.classList.contains('apagar')) {
+        elemento.parentElement.remove()
+        salvarTarefas()
+    }
+})
+
+function salvarTarefas() {
+    const liTarefas = tarefas.querySelectorAll('li')
+    const listaDeTarefas = []
+
+    for (let tarefa of liTarefas) {
+        let tarefaTexto =  tarefa.innerText
+        tarefaTexto = tarefaTexto.replace('Apagar', '').trim() // o trim não deixa espaços em branco.
+        listaDeTarefas.push(tarefaTexto)
+    }
+
+    const tarefasJSON = JSON.stringify(listaDeTarefas)
+    localStorage.setItem('tarefas', tarefasJSON) // local no navegador onde posso salvar coisas
+    
+}
+
+function carregarTarefasSalvas() {
+    const tarefas = localStorage.getItem('tarefas')
+    const listaDeTarefas = JSON.parse(tarefas);
+
+    for (let tarefa of listaDeTarefas) {
+        criaTarefa(tarefa)
+    }
+}
+
+carregarTarefasSalvas()
